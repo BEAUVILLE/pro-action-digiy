b61fbd0c93069193f44ede1b851066aadad134cd
DIGIY LYFE <baptistejb24@gmail.com>
2026-08-01 18:29:50 +0000

    Sécuriser le chargement de l’annuaire Nazir

--- annuaire-public-digiy.js
+++ annuaire-public-digiy.js
@@ -7,10 +7,10 @@
   "use strict";
 
   document.write(
-    '<script src="https://cdn.jsdelivr.net/gh/BEAUVILLE/pro-action-digiy@2a39840714109edd2d61e540189a8cdc695663d9/annuaire-public-digiy.js"><\\/script>'
+    '<script src="https://cdn.jsdelivr.net/gh/BEAUVILLE/pro-action-digiy@2a39840714109edd2d61e540189a8cdc695663d9/annuaire-public-digiy.js"></script>'
   );
 
   document.write(
-    '<script src="./nazir-action-pro-patch.js?v=20260801-nazir-driver"><\\/script>'
+    '<script src="./nazir-action-pro-patch.js?v=20260801-nazir-driver"></script>'
   );
 })();
