7eac560a39cfc24004a44135cf25a5cc107dd30f
DIGIY LYFE <baptistejb24@gmail.com>
2026-08-26 14:51:26 +0000

    Force le nouveau chargeur ACTION PRO sans FG NAILS

--- annuaire-public-digiy.js
+++ annuaire-public-digiy.js
@@ -1,6 +1,6 @@
-/* ACTION PRO — chargeur annuaire + nettoyage navigation publique — 2026-08-15 */
+/* ACTION PRO — chargeur annuaire + nettoyage navigation publique — 2026-08-26 */
 (function(){
   'use strict';
-  document.write('<script src="./annuaire-public-digiy-core.js?v=20260811-world8-v2"><\/script>');
+  document.write('<script src="./annuaire-public-digiy-core.js?v=20260826-fg-retire-v3"><\/script>');
   document.write('<script>(function(){var a=document.querySelector("a[href^=\\"https://digiy-hub.digiylyfe.com/\\"]");if(a)a.remove();})();<\/script>');
 })();
