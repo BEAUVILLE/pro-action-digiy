de32b042bd85a2daa0da20589b2fa04a2c4f0850
DIGIY LYFE <baptistejb24@gmail.com>
2026-09-01 13:27:03 +0000

    Force le nouveau choix Sénégal Europe

--- annuaire-public-digiy.js
+++ annuaire-public-digiy.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
   document.write('<script src="./annuaire-public-digiy-core.js?v=20260826-fg-retire-v3"><\/script>');
-  document.write('<script src="./voice-territory-rails-v1.js?v=20260829-country-zone-v1"><\/script>');
+  document.write('<script src="./voice-territory-rails-v1.js?v=20260901-europe-entry-v1"><\/script>');
   document.write('<script src="./voice-territory-intent-filter-v1.js?v=20260829-senegal-intent-v1"><\/script>');
   document.write('<script src="./subscription-public-gate.js?v=20260826-subscription-gate-v1"><\/script>');
   document.write('<script>(function(){var a=document.querySelector("a[href^=\\"https://digiy-hub.digiylyfe.com/\\"]");if(a)a.remove();})();<\/script>');
