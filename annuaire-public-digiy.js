8303ad85474f129b2e1f193ddc46a492b980358d
DIGIY LYFE <baptistejb24@gmail.com>
2026-09-12 18:37:34 +0000

    Update script source for voice-territory-rails

--- annuaire-public-digiy.js
+++ annuaire-public-digiy.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
   document.write('<script src="./annuaire-public-digiy-core.js?v=20260826-fg-retire-v3"><\/script>');
-  document.write('<script src="./voice-territory-rails-v1.js?v=20260912-world-territories-v1"><\/script>');
+  document.write('<script src="./voice-territory-rails-v1.js?v=20260912-mobile-scroll-v2"><\/script>');
   document.write('<script src="./voice-territory-intent-filter-v1.js?v=20260906-intent-territory-v2"><\/script>');
   document.write('<script src="./subscription-public-gate.js?v=20260826-subscription-gate-v1"><\/script>');
   document.write('<script>(function(){var a=document.querySelector("a[href^=\\"https://digiy-hub.digiylyfe.com/\\"]");if(a)a.remove();})();<\/script>');
